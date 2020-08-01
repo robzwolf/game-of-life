65625507215a35af1e491a49f5ba3aaf50c1eb1c
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-01 15:33:12 +0200

    Copy implementation 1 into implementation 2

---
+++ src/implementation-2/src/js/entry.js
@@ -0,0 +1,9 @@
+import '../styles/entry.scss';
+import Render from './render';
+import ComputationEngine from './computation-engine';
+
+const app = document.querySelector('.app');
+const playPauseButton = document.querySelector('.play-pause');
+
+const renderEngine = new Render(app, { cellSize: 40 });
+const computationEngine = new ComputationEngine(app, { playPauseButton, body: document.body, app });
