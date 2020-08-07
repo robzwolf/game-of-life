4d2c75f3af050c271efe346730ef4c642a38cd92
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-07 17:25:20 +0100

    Render simple 'Hello World' text using React

--- src/implementation-2/src/js/entry.js
+++ src/implementation-2/src/js/entry.js
@@ -1,11 +1,16 @@
 import '../styles/entry.scss';
-import Render from './render';
-import ComputationEngine from './computation-engine';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Render from './render';
+// import ComputationEngine from './computation-engine';
 
-import Form from "./react-components/Form";
+// import Form from "./react-components/Form";
 
-const app = document.querySelector('.app');
-const playPauseButton = document.querySelector('.play-pause');
+// const app = document.querySelector('.app');
+// const playPauseButton = document.querySelector('.play-pause');
 
-const renderEngine = new Render(app, { cellSize: 40 });
-const computationEngine = new ComputationEngine(app, { playPauseButton, body: document.body, app });
+// const renderEngine = new Render(app, { cellSize: 40 });
+// const computationEngine = new ComputationEngine(app, { playPauseButton, body: document.body, app });
+
+import GameOfLife from './react-components/GameOfLife'
+ReactDOM.render(<GameOfLife />, document.querySelector('.app'));
---
+++ src/implementation-2/src/js/react-components/GameOfLife.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GameOfLife extends React.Component {
+    render() {
+        return <b>Hello World</b>
+    }
+}
+
+export default GameOfLife
