38c46799d39a4554d7fc7ad4c0ab9aca92ac1e3c
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-08 12:56:40 +0100

    Grid.jsx passes ESLint

--- src/implementation-2/src/js/react-components/Grid.jsx
+++ src/implementation-2/src/js/react-components/Grid.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Row from "./Row";
 
 class Grid extends React.PureComponent {
@@ -11,31 +12,49 @@ class Grid extends React.PureComponent {
     handleClick(event) {
         const cell = event.target;
         const { xCoord, yCoord } = cell.dataset;
-        const alive = this.props.board[yCoord][xCoord];
-        this.props.setCellState(xCoord, yCoord, !alive);
+        const { board, setCellState } = this.props;
+        const alive = board[yCoord][xCoord];
+        setCellState(xCoord, yCoord, !alive);
     }
 
     render() {
+        const { handleMouseDown, handleMouseUp, height, width, board, oldBoard, setCellState, handleCellMouseEnter } = this.props;
         return (
-            <div
-                className="cells"
-                onMouseDown={(e) => this.props.handleMouseDown(e)}
-                onMouseUp={(e) => this.props.handleMouseUp(e)}
-            >
-                {[...new Array(this.props.height)].map((_, i) =>
-                    <Row
-                        width={this.props.width}
-                        rowIndex={i}
-                        key={`row-${i}`}
-                        rowBoard={this.props.board[i]}
-                        oldRowBoard={[...this.props.oldBoard[i]]}
-                        setCellState={this.props.setCellState}
-                        handleCellMouseEnter={this.props.handleCellMouseEnter}
-                    />
-                )}
-            </div>
+            <>
+                {/* ESLint rule disabled because this div contains children which are buttons */}
+                {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+                <div
+                    className="cells"
+                    onMouseDown={(e) => handleMouseDown(e)}
+                    onMouseUp={(e) => handleMouseUp(e)}
+                >
+                    {[...new Array(height)].map((_, i) =>
+                        <Row
+                            width={width}
+                            rowIndex={i}
+                            key={`row-${i}` /* eslint-disable-line react/no-array-index-key */}
+                            rowBoard={board[i]}
+                            oldRowBoard={[...oldBoard[i]]}
+                            setCellState={setCellState}
+                            handleCellMouseEnter={handleCellMouseEnter}
+                        />
+                    )}
+                </div>
+            </>
         )
+        // See https://github.com/yannickcr/eslint-plugin-react/issues/1123#issuecomment-288802108 for react/no-array-index-key
     }
 }
 
 export default Grid;
+
+Grid.propTypes = {
+    board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.bool)).isRequired,
+    setCellState: PropTypes.func.isRequired,
+    handleMouseDown: PropTypes.func.isRequired,
+    handleMouseUp: PropTypes.func.isRequired,
+    handleCellMouseEnter: PropTypes.func.isRequired,
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    oldBoard: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.bool)).isRequired,
+}
