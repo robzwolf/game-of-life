3a0a9247b27a24e42000f526bdb535127e58282b
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-08 13:06:43 +0100

    ModeNotification.jsx passes ESLint

--- src/implementation-2/src/js/react-components/ModeNotification.jsx
+++ src/implementation-2/src/js/react-components/ModeNotification.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class ModeNotification extends React.Component {
-    render() {
-        return (
-            <aside className={`${this.props.mode}-mode mode-notification`}>
-                <span>
-                    {this.props.mode}
-                    {' '}
-                    mode
-                </span>
-            </aside>
-        )
-    }
+const ModeNotification = ({ mode }) => {
+    return (
+        <aside className={`${mode}-mode mode-notification`}>
+            <span>
+                {mode}
+                {' '}
+                mode
+            </span>
+        </aside>
+    )
 }
 
 export default ModeNotification;
+
+ModeNotification.propTypes = {
+    mode: PropTypes.string.isRequired,
+}
