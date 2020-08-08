9dd1feeac67b754721c1e68e5c093b919cbcda2c
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-08 13:02:37 +0100

    Cell.jsx passes ESLint

--- src/implementation-2/src/js/react-components/Cell.jsx
+++ src/implementation-2/src/js/react-components/Cell.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Cell extends React.PureComponent {
     constructor(props) {
@@ -8,19 +9,29 @@ class Cell extends React.PureComponent {
     }
 
     toggleAlive() {
-        this.props.setCellState(this.props.xCoord, this.props.yCoord, !this.props.isAlive);
+        const { setCellState, xCoord, yCoord, isAlive } = this.props;
+        setCellState(xCoord, yCoord, !isAlive);
     }
 
     render() {
+        const { handleCellMouseEnter, xCoord, yCoord, isAlive } = this.props;
         return (
             <div
-                className={["cell", this.props.isAlive && "alive"].join(" ")}
-                onMouseEnter={(e) => this.props.handleCellMouseEnter(e)}
-                data-x-coord={this.props.xCoord}
-                data-y-coord={this.props.yCoord}
+                className={["cell", isAlive && "alive"].join(" ")}
+                onMouseEnter={(e) => handleCellMouseEnter(e)}
+                data-x-coord={xCoord}
+                data-y-coord={yCoord}
             />
         )
     }
 }
 
 export default React.memo(Cell);
+
+Cell.propTypes = {
+    setCellState: PropTypes.func.isRequired,
+    handleCellMouseEnter: PropTypes.func.isRequired,
+    xCoord: PropTypes.number.isRequired,
+    yCoord: PropTypes.number.isRequired,
+    isAlive: PropTypes.bool.isRequired
+}
