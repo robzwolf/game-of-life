1ba6fb57c879ce499d842419350a5f54c7bb083f
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-09 14:09:40 +0100

    [IM1] Add function for running arbitrary number of evaluations

--- src/implementation-1/src/js/entry.js
+++ src/implementation-1/src/js/entry.js
@@ -7,3 +7,10 @@ const playPauseButton = document.querySelector('.play-pause');
 
 const renderEngine = new Render(app, { cellSize: 40 });
 const computationEngine = new ComputationEngine(app, { playPauseButton, body: document.body, app });
+
+// Functions for testing and evaluation
+window.iterations = (numIterations) => {
+    for (let i = 0; i < numIterations; i += 1) {
+        computationEngine.computeNextIteration();
+    }
+}
