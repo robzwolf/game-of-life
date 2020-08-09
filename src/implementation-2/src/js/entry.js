0b817fe8526210c3a1f97ed705360229341732d9
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2020-08-09 14:06:26 +0100

    [IM2] Add function for running arbitrary number of evaluations

--- src/implementation-2/src/js/entry.js
+++ src/implementation-2/src/js/entry.js
@@ -4,3 +4,10 @@ import ReactDOM from "react-dom";
 import GameOfLife from "./react-components/GameOfLife";
 
 ReactDOM.render(<GameOfLife />, document.querySelector(".mount-point"));
+
+// Functions for testing and evaluation
+window.iterations = (numIterations) => {
+    for (let i = 0; i < numIterations; i += 1) {
+        window.nextIteration();
+    }
+}
